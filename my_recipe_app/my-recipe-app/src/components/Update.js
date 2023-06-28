ca58dd77a54f3470953161a31776149f119f5c0f
amianki <ankita.mandal201@gmail.com>
2023-06-29 01:21:03 +0530

    modify update function

--- my_recipe_app/my-recipe-app/src/components/Update.js
+++ my_recipe_app/my-recipe-app/src/components/Update.js
@@ -1,41 +1,51 @@
 import { Button, Form } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useLocation } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom/dist";
 const Update = () => {
     const use_location = useLocation();
     const id = use_location.state.id;
-    let name = use_location.state.name;
-    let location = use_location.state.location;
+    let dname = use_location.state.name;
+    let dlocation = use_location.state.location;
+    const history = useNavigate();
 
-    function handleSubmit(id,name,location) {
+    function handleSubmit() {
         const requestOptions = {
             method: 'PUT',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ id:id, name:name, location:location })
+            body: JSON.stringify({ id: id, name: name, location: location })
         };
-        const url = "http://localhost:4000/company/"+id
+        const url = "http://localhost:4000/company/" + id
         fetch(url, requestOptions)
-            .then(response => response.json())
-            .then(data => this.setState({ postId: data.id }));
-        
+            .then(console.log)
+        history('/')
     }
-    let state = {
-        id:"",
-        name:"",
-        location:""
-    }
-    return ( 
+    
+    const[name, setName] = useState(dname);
+    const [location, setLoc] = useState(dlocation);
+    return (
         <Form className="d-grid gap-2"
-        style={{margin:"15rem"}}>
-           <Form.Group className="mb-3" controlId="formName">
-               <Form.Control type="text" defaultValue={name} required >
-               </Form.Control>
-               <Form.Control type="text" defaultValue={location} required >
-               </Form.Control>
-           </Form.Group>
-           <Button onClick={(e) => handleSubmit(e)} type="submit">Update</Button>
-       </Form>
-     );
+            style={{ margin: "15rem" }}>
+            <Form.Group className="mb-3" controlId="formName">
+                <Form.Control
+                    type="text"
+                    value={name}
+                    required
+                    onChange={(e) => setName(e.target.value)}
+                >
+                </Form.Control>
+                <Form.Control
+                    type="text"
+                    value={location}
+                    required
+                    onChange={(e) => setLoc(e.target.value)}
+                >
+                </Form.Control>
+            </Form.Group>
+            <Button onClick={(e) => handleSubmit(e)} type="submit">Update</Button>
+        </Form>
+    );
 }
- 
+
 export default Update;
